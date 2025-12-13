e9fb4aa7eaddb399cb89bee884eae44ad0b7a3b7
Garuda-99 <165791347+Garuda-99@users.noreply.github.com>
2025-12-13 23:13:12 +0530

    v1: initial React project setup (Vite)

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+
+export default function App() {
+  return (
+    <>
+      <div></div>
+      <Header />
+      <Hero />
+    </>
+  );
+}
---
+++ src/components/Hero.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <>
+      <div>
+        <section
+          className="h-screen bg-gradient-to-b from-violet-900 to-black flex 
+        xl:flex-row flex-col-reverse items-center justify-between lg:px-24 px-10 
+        relative overflow-hidden"
+        >
+          {/* Left Section */}
+          <div>
+            <h1>
+              Building Fast <br />
+            </h1>
+          </div>
+        </section>
+      </div>
+    </>
+  );
+};
+
+export default Hero;
